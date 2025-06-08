94e149edb0aeb8defd88fc6ab91d7e19976c4331
karolina-suchomska <xrage6@gmail.com>
2025-06-08 11:43:23 +0200

    favicon and data download

--- components/BusinessCardProfile.vue
+++ components/BusinessCardProfile.vue
@@ -2,7 +2,7 @@
   <div class="business-card__profile">
     <img
       class="business-card__avatar"
-      :src="`${user.image.baseUrl}${user.image.filename}.${user.image.extension}`"
+      :src="imageSrc"
       :alt="user.name"
     />
     <div class="business-card__info">
@@ -14,7 +14,7 @@
 
       <PhoneReveal :phone-number="user.phone" />
 
-      <button class="business-card__button-main business-card__download">
+      <button class="business-card__button-main business-card__download"  @click="handleDownload">
         Pobierz dane użytkownika
       </button>
     </div>
@@ -22,9 +22,12 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+
+import avatarPlaceholder from '@/assets/img/avatar-placeholder.png';
 import PhoneReveal from './PhoneReveal.vue';
 
-defineProps<{
+const props = defineProps<{
   user: {
     name: string
     surname: string
@@ -34,7 +37,28 @@ defineProps<{
       baseUrl: string
       filename: string
       extension: string
-    }
+    },
+    about: string
   }
 }>();
+
+const imageSrc = computed(() => {
+  const img = props.user.image
+  return img?.baseUrl && img.filename && img.extension ? `${img.baseUrl}${img.filename}.${img.extension}` : avatarPlaceholder
+});
+
+const handleDownload = () => {
+  const url = URL.createObjectURL(
+    new Blob([JSON.stringify(props.user, null, 2)], {
+      type: 'application/json'
+    })
+  )
+
+  Object.assign(document.createElement('a'), {
+    href: url,
+    download: 'user-data.json'
+  }).click()
+
+  URL.revokeObjectURL(url)
+}
 </script>
